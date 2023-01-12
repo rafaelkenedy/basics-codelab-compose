9d34ee5b015f8c1373dc2c391c4309c3f523a39a
rafael <k.rafaelalves@gmail.com>
2023-01-12 17:34:34 -0300

    We have animation and a big list!

--- app/src/main/java/com/rafael/basicscodelabcompose/MainActivity.kt
+++ app/src/main/java/com/rafael/basicscodelabcompose/MainActivity.kt
@@ -4,9 +4,14 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,13 +40,16 @@ fun MyApp() {
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("World", "Compose")) {
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
     Surface(
         color = MaterialTheme.colors.background
     ) {
         Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            for (name in names) {
-                Greeting(name)
+            LazyColumn {
+                item { Text("Header") }
+                items(names) { name ->
+                    Greeting(name)
+                }
             }
         }
     }
@@ -49,8 +57,13 @@ fun Greetings(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val expanded = rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = tween(
+            durationMillis = 1000
+        )
+    )
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
