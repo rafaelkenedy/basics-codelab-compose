ad1a6de7cc7ab57a420acf78aae375007367736f
rafael <k.rafaelalves@gmail.com>
2023-01-12 17:50:00 -0300

    We have a spring animation!

--- app/src/main/java/com/rafael/basicscodelabcompose/MainActivity.kt
+++ app/src/main/java/com/rafael/basicscodelabcompose/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,7 +36,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = !shouldShowOnboarding })
     } else {
@@ -60,9 +65,13 @@ fun Greeting(name: String) {
     val expanded = rememberSaveable { mutableStateOf(false) }
     val extraPadding by animateDpAsState(
         targetValue = if (expanded.value) 48.dp else 0.dp,
-        animationSpec = tween(
-            durationMillis = 1000
-        )
+//        animationSpec = tween(
+//            durationMillis = 1000
+//        )
+          animationSpec = spring(
+              dampingRatio = Spring.DampingRatioHighBouncy,
+              stiffness = Spring.StiffnessLow
+          )
     )
     Surface(
         color = MaterialTheme.colors.primary,
